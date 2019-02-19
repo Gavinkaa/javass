3a79385d6d69d6631cf9182c800cb3f522043680
Gavinka <gavinka.dev@gmail.com>
2019-02-19 15:22:37 +0100

    add pack to Bits32

--- src/ch/epfl/javass/Bits32.java
+++ src/ch/epfl/javass/Bits32.java
@@ -1,14 +1,15 @@
 package ch.epfl.javass;
 
 public final class Bits32 {
-    private Bits32() {}
+    private Bits32() {
+    }
 
     public static int mask(int start, int size) {
         Preconditions.checkArgument(0 <= start && start <= Integer.SIZE - size);
         Preconditions.checkArgument(0 <= size && size <= Integer.SIZE - start);
-         if (size == 32) {
-             return -1;
-         }
+        if (size == 32) {
+            return -1;
+        }
         return ((1 << size) - 1) << start;
     }
 
@@ -18,11 +19,30 @@ public final class Bits32 {
     }
 
     public static int pack(int v1, int s1, int v2, int s2) {
-        return 0;
+        int index = 0;
+        int ret = 0;
+
+        ret |= Bits32.packAt(v1, s1, index);
+        index += s1;
+
+        ret |= Bits32.packAt(v2, s2, index);
+
+        return ret;
     }
 
     public static int pack(int v1, int s1, int v2, int s2, int v3, int s3) {
-        return 0;
+        int index = 0;
+        int ret = 0;
+
+        ret |= Bits32.packAt(v1, s1, index);
+        index += s1;
+
+        ret |= Bits32.packAt(v2, s2, index);
+        index += s2;
+
+        ret |= Bits32.packAt(v3, s3, index);
+
+        return ret;
     }
 
     public static int pack(
@@ -30,6 +50,35 @@ public final class Bits32 {
             int v4, int s4, int v5, int s5, int v6, int s6,
             int v7, int s7
     ) {
-        return 0;
+        int index = 0;
+        int ret = 0;
+
+        ret |= Bits32.packAt(v1, s1, index);
+        index += s1;
+
+        ret |= Bits32.packAt(v2, s2, index);
+        index += s2;
+
+        ret |= Bits32.packAt(v3, s3, index);
+        index += s3;
+
+        ret |= Bits32.packAt(v4, s4, index);
+        index += s4;
+
+        ret |= Bits32.packAt(v5, s5, index);
+        index += s5;
+
+        ret |= Bits32.packAt(v6, s6, index);
+        index += s6;
+
+        ret |= Bits32.packAt(v7, s7, index);
+        return ret;
+    }
+
+    private static int packAt(int v, int s, int index) {
+        Preconditions.checkArgument(1 <= s && s < Integer.SIZE);
+        Preconditions.checkArgument(0 <= index && index + s <= Integer.SIZE);
+        Preconditions.checkArgument(v >>> s == 0);
+        return Bits32.extract(v, 0, s) << index;
     }
 }
