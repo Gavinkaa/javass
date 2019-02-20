a18fbeb3dd1f35e31d29e9f46958adbd15e0c849
Lúcás Meier <cronokirby@gmail.com>
2019-02-20 15:05:58 +0100

    Add Card.Rank enum

--- src/ch/epfl/javass/jass/Card.java
+++ src/ch/epfl/javass/jass/Card.java
@@ -25,4 +25,36 @@ public final class Card {
             return symbol;
         }
     }
+
+    public enum Rank {
+        SIX("6", 0),
+        SEVEN("7", 1),
+        EIGHT("8", 2),
+        NINE("9", 7),
+        TEN("10", 3),
+        JACK("J", 8),
+        QUEEN("Q", 4),
+        KING("K", 5),
+        ACE("A", 6);
+
+        private String repr;
+        private int trumpOrdinal;
+
+        Rank(String repr, int trumpOrdinal) {
+            this.repr = repr;
+            this.trumpOrdinal = trumpOrdinal;
+        }
+
+        public static final List<Color> ALL = Collections.unmodifiableList(Arrays.asList(Color.values()));
+        public static final int COUNT = ALL.size();
+
+        @Override
+        public String toString() {
+            return repr;
+        }
+
+        int trumpOrdinal() {
+            return trumpOrdinal;
+        }
+    }
 }
