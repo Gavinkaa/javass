0ea4a5bcdab7533c66378f3871ea463bb06839d8
Lúcás Meier <cronokirby@gmail.com>
2019-02-20 14:47:11 +0100

    Add Jass interface with useful constants'

---
+++ src/ch/epfl/javass/jass/Jass.java
@@ -0,0 +1,12 @@
+package ch.epfl.javass.jass;
+
+/**
+ * Contains useful constants for the game of Jass
+ */
+public interface Jass {
+    int HAND_SIZE = 9;
+    int TRICKS_PER_TURN = 9;
+    int WINNING_POINTS = 1000;
+    int MATCH_ADDITIONAL_POINTS = 100;
+    int LAST_TRICK_ADDITIONAL_POINTS = 5;
+}
