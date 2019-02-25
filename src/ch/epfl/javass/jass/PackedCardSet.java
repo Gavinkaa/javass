cf75fc5f8c181edb94e34f484eba075c9943afed
Lúcás Meier <cronokirby@gmail.com>
2019-02-25 14:59:03 +0100

    Add basic PackedCardSet structure

---
+++ src/ch/epfl/javass/jass/PackedCardSet.java
@@ -0,0 +1,8 @@
+package ch.epfl.javass.jass;
+
+public final class PackedCardSet {
+    public static final long EMPTY = 0;
+    public static final long ALL_CARDS = 0b1111_1111_0000000_1111_1111_0000000_1111_1111_0000000_1111_1111L;
+    private PackedCardSet() {
+    }
+}
