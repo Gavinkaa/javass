6f36776b476b32c8c32d048975918c6abc9c145c
Gavinkaa <ludodi2000@gmail.com>
2019-04-01 15:04:24 +0200

    vive michel, stringserializer basic stuff (diamond)

---
+++ src/ch/epfl/javass/net/StringSerializer.java
@@ -0,0 +1,46 @@
+package ch.epfl.javass.net;
+
+/**
+ * @author Lúcás Críostóir Meier (300831)
+ * @author Ludovic Burnier (301308)
+ */
+public final class StringSerializer {
+    private static final int RADIX = 16;
+    private StringSerializer() {
+    }
+
+    /**
+     * Serialize a number using the base 16 representation
+     *
+     * @param num the number to serialize
+     * @return the serialized number
+     */
+    public static String serializeInt(int num) {
+        return Integer.toUnsignedString(num, RADIX);
+    }
+
+    /**
+     * Deserialize a number from the base 16 representation
+     * this is the opposite of {@link #serializeInt(int)}
+     *
+     * @param num the text to deserialize
+     * @return the deserialized number
+     */
+    public static int deserializeInt(String num) {
+        return Integer.valueOf(num, RADIX);
+    }
+
+    /**
+     * Similar to {@link #serializeInt(int)} except it can operate on longer numbers
+     */
+    public static String serializeLong(long num) {
+        return Long.toUnsignedString(num, RADIX);
+    }
+
+    /**
+     * Similar to {@link #deserializeInt(String)} except it can operate on longer numbers
+     */
+    public static long deserializeLong(String num) {
+        return Long.valueOf(num, RADIX);
+    }
+}
---
+++ test/ch/epfl/javass/net/StringSerializerTest.java
@@ -0,0 +1,52 @@
+package ch.epfl.javass.net;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringSerializerTest {
+
+    @Test
+    void serializeIntWorksOnExample() {
+        assertEquals("1", StringSerializer.serializeInt(1));
+        assertEquals("10", StringSerializer.serializeInt(16));
+        assertEquals("20", StringSerializer.serializeInt(32));
+        assertEquals("100", StringSerializer.serializeInt(256));
+        assertEquals("b", StringSerializer.serializeInt(11));
+    }
+
+    @Test
+    void deserializeIntWorksOnExample() {
+        assertEquals(1, StringSerializer.deserializeInt("1"));
+        assertEquals(16, StringSerializer.deserializeInt("10"));
+        assertEquals(32, StringSerializer.deserializeInt("20"));
+        assertEquals(256, StringSerializer.deserializeInt("100"));
+        assertEquals(11, StringSerializer.deserializeInt("b"));
+    }
+
+    @Test
+    void serializeLong() {
+        //SAME AS INT
+        assertEquals("1", StringSerializer.serializeLong(1));
+        assertEquals("10", StringSerializer.serializeLong(16));
+        assertEquals("20", StringSerializer.serializeLong(32));
+        assertEquals("100", StringSerializer.serializeLong(256));
+        assertEquals("b", StringSerializer.serializeLong(11));
+        //---
+        assertEquals("100000000", StringSerializer.serializeLong(0x1_0000_0000L));
+        assertEquals("12345678944", StringSerializer.serializeLong(0x12345678944L));
+    }
+
+    @Test
+    void deserializeLong() {
+        //SAME AS INT
+        assertEquals(1, StringSerializer.deserializeLong("1"));
+        assertEquals(16, StringSerializer.deserializeLong("10"));
+        assertEquals(32, StringSerializer.deserializeLong("20"));
+        assertEquals(256, StringSerializer.deserializeLong("100"));
+        assertEquals(11, StringSerializer.deserializeLong("b"));
+        //---
+        assertEquals(0x1_0000_0000L, StringSerializer.deserializeLong("100000000"));
+        assertEquals(0x12345678944L, StringSerializer.deserializeLong("12345678944"));
+    }
+}
