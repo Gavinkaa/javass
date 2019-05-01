94c8de70b5537d8da0359cf23f74c200622ae64e
Gavinkaa <ludodi2000@gmail.com>
2019-05-01 17:38:02 +0200

    Score corrected

--- src/ch/epfl/javass/jass/Score.java
+++ src/ch/epfl/javass/jass/Score.java
@@ -101,7 +101,7 @@ public final class Score {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Score)) return false;
         Score score = (Score) o;
         return packed == score.packed;
     }
