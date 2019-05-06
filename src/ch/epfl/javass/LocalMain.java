126d8b3116ceb2667ddc4f09039ad58bba072455
Lúcás Meier <cronokirby@gmail.com>
2019-05-06 15:04:20 +0200

    WIP: Add PlayerBuilder

---
+++ src/ch/epfl/javass/LocalMain.java
@@ -0,0 +1,49 @@
+package ch.epfl.javass;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.List;
+import java.util.Random;
+
+public final class LocalMain extends Application {
+    private void fatal(String message) {
+        System.err.println("Erreur : " + message);
+        System.exit(1);
+    }
+
+    private long parseNumber(String number) {
+        try {
+            return Long.parseLong(number);
+        } catch (NumberFormatException e) {
+            fatal("Invalid Number: " + e.toString());
+        }
+        throw new RuntimeException("Unreachable code");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        List<String> args = getParameters().getRaw();
+        int size = args.size();
+        if (size != 4 && size != 5) {
+            fatal("Expected 4 or 5 arguments got: " + size);
+        }
+        Random rng;
+        if (size == 5) {
+            rng = new Random(parseNumber(args.get(4)));
+        } else {
+            rng = new Random();
+        }
+        long jassGameSeed = rng.nextLong();
+        try (PlayerBuilder pb = new PlayerBuilder()) {
+            for (int i = 0; i < 4; ++i) {
+                String msg = pb.nextPlayer(rng.nextLong(), args.get(i));
+                if (msg != null) fatal(msg + " : " + args.get(i));
+            }
+        }
+    }
+}
---
+++ src/ch/epfl/javass/PlayerBuilder.java
@@ -0,0 +1,116 @@
+package ch.epfl.javass;
+
+import ch.epfl.javass.gui.GraphicalPlayerAdapter;
+import ch.epfl.javass.jass.MctsPlayer;
+import ch.epfl.javass.jass.Player;
+import ch.epfl.javass.jass.PlayerId;
+import ch.epfl.javass.net.RemotePlayerClient;
+
+import java.io.IOException;
+import java.util.*;
+
+public final class PlayerBuilder implements AutoCloseable {
+    private PlayerId currentID = PlayerId.PLAYER_1;
+    private final Map<PlayerId, Player> players = new EnumMap<>(PlayerId.class);
+    private final Map<PlayerId, String> names = new EnumMap<>(PlayerId.class);
+    private final List<RemotePlayerClient> remotes = new ArrayList<>(4);
+
+    public PlayerBuilder() {
+        names.put(PlayerId.PLAYER_1, "Aline");
+        names.put(PlayerId.PLAYER_2, "Bastien");
+        names.put(PlayerId.PLAYER_3, "Colette");
+        names.put(PlayerId.PLAYER_4, "David");
+    }
+
+    public Map<PlayerId, Player> getPlayers() {
+        return Collections.unmodifiableMap(players);
+    }
+
+    public Map<PlayerId, String> getNames() {
+        return Collections.unmodifiableMap(names);
+    }
+
+    public String nextPlayer(long seed, String info) {
+        if (currentID == null) {
+            throw new IllegalStateException("No players to initialize");
+        }
+        String[] parts = info.split(":");
+        if (parts.length < 1) return "Not enough parts in player information";
+        if (parts.length > 3) return "Too many parts in player information";
+        switch (parts[0]) {
+            case "h":
+                return nextHuman(parts);
+            case "s":
+                return nextSimulated(seed, parts);
+            case "r":
+                return nextRemote(parts);
+            default:
+                return "Unrecognized player type";
+        }
+    }
+
+    private void insertNext(String name, Player player) {
+        if (!name.isEmpty()) {
+            names.put(currentID, name);
+        }
+        players.put(currentID, player);
+        if (currentID == PlayerId.PLAYER_4) {
+            currentID = null;
+        } else {
+            currentID = PlayerId.ALL.get(currentID.ordinal() + 1);
+        }
+    }
+
+    private String nextHuman(String[] parts) {
+        if (parts.length == 3) return "Too many parts for local human player";
+        String name = parts.length == 2 ? parts[1] : "";
+        Player player = new GraphicalPlayerAdapter();
+        insertNext(name, player);
+        return null;
+    }
+
+    private String nextSimulated(long seed, String[] parts) {
+        int iterations = 10000;
+        if (parts.length == 3) {
+            try {
+                iterations = Integer.parseInt(parts[2]);
+                if (iterations < 9) {
+                    return "MCTS iterations must be at least 9";
+                }
+            } catch (NumberFormatException e) {
+                return "Invalid iteration number for MCTS";
+            }
+        }
+        String name = parts.length >= 2 ? parts[1] : "";
+        Player player = new MctsPlayer(currentID, seed, iterations);
+        insertNext(name, player);
+        return null;
+    }
+
+    private String nextRemote(String[] parts) {
+        if (parts.length == 3) {
+            if (!parts[2].matches("[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}")) {
+                return "Invalid IPV4 address";
+            }
+        }
+        String name = parts.length >= 2 ? parts[1] : "";
+        String hostname = parts.length == 3 ? parts[2] : "localhost";
+        Player player;
+        try {
+            RemotePlayerClient remote = new RemotePlayerClient(hostname);
+            remotes.add(remote);
+            player = remote;
+        } catch (IOException e) {
+            return "Failed to connect to " + hostname + " : " + e.toString();
+        }
+        insertNext(name, player);
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+        for (RemotePlayerClient client : remotes) {
+            client.close();
+        }
+    }
+}
