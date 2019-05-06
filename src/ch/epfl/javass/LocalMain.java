caf238bead3dc04143f2348355ae5ca7f64bc080
Lúcás Meier <cronokirby@gmail.com>
2019-05-06 17:00:34 +0200

    Add Usage message

--- src/ch/epfl/javass/LocalMain.java
+++ src/ch/epfl/javass/LocalMain.java
@@ -1,6 +1,7 @@
 package ch.epfl.javass;
 
 import ch.epfl.javass.jass.JassGame;
+import ch.epfl.javass.net.StringSerializer;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,6 +9,18 @@ import java.util.List;
 import java.util.Random;
 
 public final class LocalMain extends Application {
+    private static String makeUsage() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Utilisation: java ch.epfl.javass.LocalMain <j1>..<j4> [graine] où :\n");
+        sb.append("  <jN> spécifie le joueur N, un de:\n");
+        sb.append("    h:<nom> un joueur humain nommé <nom>\n");
+        sb.append("    r:<nom>:<hôte> un joueur en ligne, nommé <nom>, connecté au réseau sur <host>\n");
+        sb.append("    s:<nom>:<iterations> un joueur simulé par MCTS, nommé <nom>, faisant <iterations> part décision\n");
+        sb.append(" [graine] si donné va rendre l'aléatoire du jeu déterministe, avec cette graine comme incipit\n");
+        return sb.toString();
+    }
+    private final static String USAGE = makeUsage();
+
     private void fatal(String message) {
         System.err.println("Erreur : " + message);
         System.exit(1);
@@ -31,7 +44,8 @@ public final class LocalMain extends Application {
         List<String> args = getParameters().getRaw();
         int size = args.size();
         if (size != 4 && size != 5) {
-            fatal("Expected 4 or 5 arguments got: " + size);
+            System.out.println(USAGE);
+            System.exit(1);
         }
         Random rng;
         if (size == 5) {
