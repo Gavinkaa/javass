2c4d352d0645150a482fbec9f9a44007ada75d03
Gavinkaa <ludodi2000@gmail.com>
2019-05-17 14:20:02 +0200

    Choices button set in center

--- src/ch/epfl/javass/Wizard.java
+++ src/ch/epfl/javass/Wizard.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -63,6 +64,7 @@ public class Wizard extends Application {
 
         VBox vBox = new VBox(50);
         vBox.getChildren().addAll(localButton, remoteButton);
+        vBox.setAlignment(Pos.CENTER);
 
         return vBox;
     }
