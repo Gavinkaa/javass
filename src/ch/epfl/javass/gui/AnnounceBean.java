11869bd518f6baf1be69e9694eb79c9502db0303
Lúcás Meier <cronokirby@gmail.com>
2019-05-29 13:43:49 +0200

    Create points property in bean

--- src/ch/epfl/javass/gui/AnnounceBean.java
+++ src/ch/epfl/javass/gui/AnnounceBean.java
@@ -4,10 +4,12 @@ import ch.epfl.javass.jass.Card;
 import ch.epfl.javass.jass.CardSet;
 import ch.epfl.javass.jass.PlayerId;
 import ch.epfl.javass.jass.TeamId;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.*;
 import javafx.beans.value.ObservableBooleanValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 
 import java.util.EnumMap;
 import java.util.Map;
@@ -15,12 +17,15 @@ import java.util.Map;
 public final class AnnounceBean {
     private final BooleanProperty announcesVisible = new SimpleBooleanProperty(false);
     private final Map<PlayerId, ObservableList<Card>> announces;
+    private final Map<PlayerId, SimpleIntegerProperty> points;
     private final ObjectProperty<TeamId> winningTeam = new SimpleObjectProperty<>(null);
 
     public AnnounceBean() {
         this.announces = new EnumMap<>(PlayerId.class);
+        this.points = new EnumMap<>(PlayerId.class);
         for (PlayerId pid : PlayerId.ALL) {
             this.announces.put(pid, FXCollections.observableArrayList());
+            this.points.put(pid, new SimpleIntegerProperty(0));
         }
     }
 
@@ -30,6 +35,10 @@ public final class AnnounceBean {
 
     public ObservableList<Card> announces(PlayerId player) {
         return FXCollections.unmodifiableObservableList(this.announces.get(player));
+    }
+
+    public ReadOnlyIntegerProperty points(PlayerId player) {
+        return points.get(player);
     }
 
     public ReadOnlyObjectProperty<TeamId> winningTeam() {
@@ -49,6 +58,8 @@ public final class AnnounceBean {
             for (int i = 0; i < cards.size(); ++i) {
                 list.add(cards.get(i));
             }
+            int points = cards.announceValue().points();
+            this.points.get(player).setValue(points);
         }
     }
 
